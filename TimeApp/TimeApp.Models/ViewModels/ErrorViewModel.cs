15b6377c9e58787a72026329194790a2628104c1
Blertin Hamza <blertin.hamza7@gmail.com>
2022-08-01 16:15:47 +0200

    Setup-project

---
+++ TimeApp/TimeApp.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TimeApp/TimeApp.Utility/SD.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TimeApp.Utility
+{
+    public class Class1
+    {
+    }
+}
