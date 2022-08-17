1eb96fd4f42e2b64399d83d8c480e6d3837e9bf7
Mentor Qorri <mentor-qorri@outlook.com>
2022-08-17 11:14:13 +0200

    time and interview style done

--- time.App-15b6377c9e58787a72026329194790a2628104c1/TimeApp/TimeApp/Areas/Agent/Controllers/InterviewsController.cs
+++ time.App-15b6377c9e58787a72026329194790a2628104c1/TimeApp/TimeApp/Areas/Agent/Controllers/InterviewsController.cs
@@ -27,25 +27,6 @@ namespace TimeApp.Areas.Agent.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Agent/Interviews/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var interview = await _context.Interviews
-                .Include(i => i.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (interview == null)
-            {
-                return NotFound();
-            }
-
-            return View(interview);
-        }
-
         // GET: Agent/Interviews/Create
         public IActionResult Create()
         {
@@ -63,91 +44,10 @@ namespace TimeApp.Areas.Agent.Controllers
                 interview.Data = DateTime.Now;
                 _context.Add(interview);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Create));
             }
             ViewData["User_Id"] = new SelectList(_context.Users, "Id", "Username", interview.User_Id);
             return View(interview);
-        }
-
-        // GET: Agent/Interviews/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var interview = await _context.Interviews.FindAsync(id);
-            if (interview == null)
-            {
-                return NotFound();
-            }
-            ViewData["User_Id"] = new SelectList(_context.Users, "Id", "Username", interview.User_Id);
-            return View(interview);
-        }
-
-        // POST: Agent/Interviews/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Interviews,User_Id,Data")] Interview interview)
-        {
-            if (id != interview.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(interview);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!InterviewExists(interview.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["User_Id"] = new SelectList(_context.Users, "Id", "Password", interview.User_Id);
-            return View(interview);
-        }
-
-        // GET: Agent/Interviews/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var interview = await _context.Interviews
-                .Include(i => i.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (interview == null)
-            {
-                return NotFound();
-            }
-
-            return View(interview);
-        }
-
-        // POST: Agent/Interviews/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var interview = await _context.Interviews.FindAsync(id);
-            _context.Interviews.Remove(interview);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool InterviewExists(int id)
--- time.App-15b6377c9e58787a72026329194790a2628104c1/TimeApp/TimeApp/Areas/Agent/Controllers/TimesController.cs
+++ time.App-15b6377c9e58787a72026329194790a2628104c1/TimeApp/TimeApp/Areas/Agent/Controllers/TimesController.cs
@@ -26,26 +26,7 @@ namespace TimeApp.Areas.Agent.Controllers
             var applicationDbContext = _context.Times.Include(t => t.User);
             return View(await applicationDbContext.ToListAsync());
         }
-
-        // GET: Agent/Times/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var time = await _context.Times
-                .Include(t => t.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (time == null)
-            {
-                return NotFound();
-            }
-
-            return View(time);
-        }
-
+      
         // GET: Agent/Times/Create
         public IActionResult Create()
         {
@@ -63,91 +44,10 @@ namespace TimeApp.Areas.Agent.Controllers
                 time.DateTime = DateTime.Now;
                 _context.Add(time);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Create));
             }
             ViewData["User_Id"] = new SelectList(_context.Users, "Id", "Username", time.User_Id);
             return View(time);
-        }
-
-        // GET: Agent/Times/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var time = await _context.Times.FindAsync(id);
-            if (time == null)
-            {
-                return NotFound();
-            }
-            ViewData["User_Id"] = new SelectList(_context.Users, "Id", "Username", time.User_Id);
-            return View(time);
-        }
-
-        // POST: Agent/Times/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,DateTime,User_Id")] Time time)
-        {
-            if (id != time.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(time);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TimeExists(time.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["User_Id"] = new SelectList(_context.Users, "Id", "Username", time.User_Id);
-            return View(time);
-        }
-
-        // GET: Agent/Times/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var time = await _context.Times
-                .Include(t => t.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (time == null)
-            {
-                return NotFound();
-            }
-
-            return View(time);
-        }
-
-        // POST: Agent/Times/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var time = await _context.Times.FindAsync(id);
-            _context.Times.Remove(time);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool TimeExists(int id)
