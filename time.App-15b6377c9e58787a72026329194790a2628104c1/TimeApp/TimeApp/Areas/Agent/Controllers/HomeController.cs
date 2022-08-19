39d70c794bb5163c4cde47c247672a5fd4af23ec
Mentor Qorri <mentor-qorri@outlook.com>
2022-08-19 11:34:35 +0200

    Login almost done

--- time.App-15b6377c9e58787a72026329194790a2628104c1/TimeApp/TimeApp/Areas/Agent/Controllers/HomeController.cs
+++ time.App-15b6377c9e58787a72026329194790a2628104c1/TimeApp/TimeApp/Areas/Agent/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using TimeApp.Models;
 namespace TimeApp.Areas.Agent.Controllers
 {
     [Area("Agent")]
+    [Authorize(Roles = "Agent, Admin")]
     public class HomeController : Controller
     {
       
